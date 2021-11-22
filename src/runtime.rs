a4d27e014b07a929cf6266c0bb295332ea9ad571
willtunnels <bendriscoll00@gmail.com>
2021-11-21 21:49:56 -0800

    Work out a full example of using the library
    Co-authored-by: FddTyan <FddTyan@users.noreply.github.com>

--- src/runtime.rs
+++ src/runtime.rs
@@ -145,7 +145,7 @@ where
     fn eval() -> F;
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Serialize)]
 struct IVec<T> {
     items: Vec<T>,
 }
@@ -213,19 +213,170 @@ impl<T: IType> IType for IVec<T> {
     }
 }
 
+#[derive(Clone, Debug)]
+struct Replace<T> {
+    val: T,
+}
+
+#[derive(Clone, Debug)]
+enum ReplaceAction<T: IType> {
+    Replace(T),
+    Modify(T::Action),
+}
+
+impl<T: IType> IType for Replace<T> {
+    type Action = ReplaceAction<T>;
+
+    fn id(x: Self) -> Self::Action {
+        ReplaceAction::Modify(T::id(x.val))
+    }
+
+    fn is_id_hint(a: Self::Action) -> bool {
+        use ReplaceAction as A;
+        match a {
+            // TODO: require `IType: Eq`?
+            A::Replace(_) => false,
+            A::Modify(a) => T::is_id_hint(a),
+        }
+    }
+
+    fn compose(a1: Self::Action, a2: Self::Action) -> Option<Self::Action> {
+        use ReplaceAction as A;
+        Some(match (a1, a2) {
+            (_, A::Replace(a2)) => A::Replace(a2),
+            (A::Replace(a1), A::Modify(a2)) => A::Replace(a1.apply(a2).unwrap()), // TODO: review
+            (A::Modify(a1), A::Modify(a2)) => A::Modify(T::compose(a1, a2).unwrap()),
+        })
+    }
+
+    fn apply(self, a: Self::Action) -> Option<Self> {
+        use ReplaceAction as A;
+        Some(match a {
+            A::Replace(a) => Replace { val: a },
+            A::Modify(a) => Replace {
+                val: self.val.apply(a).unwrap(),
+            },
+        })
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
 struct VecGet;
 
 #[derive(Clone, Debug, Serialize)]
 struct VecGetState<T> {
+    vec: IVec<T>,
     idx: usize,
-    phantom: PhantomData<T>,
 }
 
-impl<T: IType> IState<(IVec<T>, Stateless<usize>), T> for VecGetState<T> {
-    fn next(&mut self, action: <(IVec<T>, Stateless<usize>) as IType>::Action) -> T::Action {
-        todo!()
+type Idx = Stateless<usize>;
+
+impl<T: IType + Serialize> IState<(IVec<T>, Idx), Replace<T>> for VecGetState<T> {
+    fn next(&mut self, a: <(IVec<T>, Idx) as IType>::Action) -> ReplaceAction<T> {
+        if a.1 == self.idx {
+            match a.0.updates.get(&a.1) {
+                None => ReplaceAction::Modify(T::id(self.vec.items[a.1])),
+                Some(item_a) => ReplaceAction::Modify(item_a.clone()),
+            }
+        } else {
+            let ret_a = ReplaceAction::Replace(self.vec.items[a.1].clone());
+            if let Some(item_a) = a.0.updates.get(&a.1) {
+                ret_a = Replace::compose(ret_a, ReplaceAction::Modify(item_a.clone())).unwrap();
+            }
+            ret_a
+        }
     }
 }
+
+/*
+
+////////////////////////////////////////
+// Written in our langague
+////////////////////////////////////////
+
+extern IVec<T> = ::crate::runtime::IVec;
+fn sum(vec: IVec<i32>, i: Stateless<i32>, j: Stateless<i32>) -> i32 {
+   add(vec_get(vec, i), vec_get(vec, j))
+}
+
+////////////////////////////////////////
+// Compiler output (which is Rust code):
+////////////////////////////////////////
+
+#[derive(Clone, Debug, Serialize)]
+IStateSum {
+    s0: VecGetState<Stateless<i32>>,
+    s1: VecGetState<Stateless<i32>>,
+    s2: AddState<Stateless<i32>, Stateless<i32>>,
+}
+
+impl IState<(IVec<Stateless<i32>>, Stateless<i32>, Stateless<i32>), Stateless<i32>> for IStateSum {
+    fn next(&mut self, a: (IVec<Stateless<i32>>, Stateless<i32>, Stateless<i32>)::Action) -> Stateless<i32>::Action {
+        let fst = self.s0.next((a.0.clone(), a.1));
+        let snd = self.s1.next((a.0, a.2));
+        self.next((fst, snd))
+    }
+}
+
+#[derive(Clone, Debug, Serialize, Default)]
+IFnSum {
+    f0: VecGetFn<Stateless<i32>>,
+    f1: VecGetFn<Stateless<i32>>,
+    f2: AddFn<Stateless<i32>, Stateless<i32>>,
+}
+
+impl IFnSum {
+    fn new() -> Self {
+        Self {
+            f0: VecGetFn::new(),
+            f1: VecGetFn::new(),
+            f2: AddFn::new(),
+        }
+    }
+}
+
+impl IFn<(IVec<Stateless<i32>>, Stateless<i32>, Stateless<i32>), Stateless<i32>> for IFnSum {
+    fn init(self, args: (IVec<Stateless<i32>>, Stateless<i32>, Stateless<i32>)) -> (IStateSum, Stateless<i32>) {
+        let (s0, fst) = self.f0.init((args.0.clone(), args.1.clone()));
+        let (s1, snd) = self.f1.init((args.0.clone(), args.2.clone()));
+        let (s2, res) = self.f2.init((args.1, args.2));
+        (IStateSum { s0, s1, s2 }, res)
+    }
+}
+
+////////////////////////////////////////
+// Written in Rust
+////////////////////////////////////////
+
+include_source!(inc);
+fn make_change() {
+    // Create the initial state
+    let v: IVec<Stateless<i32>> = IVec { items: vec![0, 1, 2] };
+    let i = Stateless { val: 0 }; // A Stateless<i32> action is an i32
+    let j = Stateless { val: 1 };
+
+    // Run the pipeline
+    let sum = inc::IFnSum::new();
+    let (res, state) = sum.init((v, i, j));
+
+    // Create a diff on the initial state
+    let mut modifications = HashMap::new();
+    modifications.insert(0, 7);
+    let a = IVecAction { modifications, pushes: vec![...] }
+
+    // Rerun the pipeline incrementally
+    let new_res = res.apply(state.next((a, 0, 1)));
+}
+
+////////////////////////////////////////
+// A more interesting example
+////////////////////////////////////////
+
+fn sum(vec: IVec<i32>, i: i32, j: i32) -> i32 {
+   add(expensive(vec_get(vec, i)), expensive(vec_get(vec, j)))
+}
+
+*/
 
 // ...
 // let x = get(vec, i)
@@ -233,9 +384,36 @@ impl<T: IType> IState<(IVec<T>, Stateless<usize>), T> for VecGetState<T> {
 
 // get(vec: IVec<T>, index_to_remove: usize) -> T
 
-impl<T: IType> IFn<(IVec<T>, Stateless<usize>), T, VecGetState<T>> for VecGet {
-    fn init(self, input: (IVec<T>, Stateless<usize>)) -> (VecGetState<T>, T) {
-        todo!()
+// impl<T: IType + Serialize> IFn<(IVec<T>, EqGuard<Idx>), Replace<T>, VecGetState<T>> for VecGet {
+//     fn init(self, a: (IVec<T>, EqGuard<Idx>)) -> (VecGetState<T>, Replace<T>) {
+//         let val = a.0.items[a.1.val.val].clone();
+//         (
+//             VecGetState {
+//                 vec: a.0,
+//                 idx: a.1.val.val,
+//             },
+//             Replace { val },
+//         )
+//     }
+// }
+
+impl<T: IType> IType for Box<T> {
+    type Action = T::Action;
+
+    fn id(x: Self) -> Self::Action {
+        T::id(*x)
+    }
+
+    fn is_id_hint(a: Self::Action) -> bool {
+        T::is_id_hint(a)
+    }
+
+    fn compose(a1: Self::Action, a2: Self::Action) -> Option<Self::Action> {
+        T::compose(a1, a2)
+    }
+
+    fn apply(self, a: Self::Action) -> Option<Self> {
+        (*self).apply(a).map(Box::new)
     }
 }
 
